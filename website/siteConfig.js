1fada0e66d4df3d530baaab977ef0bc2a38b8044
Octogonapus <firey45@gmail.com>
2019-12-27 23:55:49 -0500

    Add versions and reconfigure domain

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: 'Common Wealth Robotics', // Title for your website.
   tagline: 'Learn Programming with Robotics',
-  url: 'https://commonwealthrobotics.github.io', // Your website URL
-  baseUrl: '/CommonWealthRobotics.github.io/', // Base URL for your project */
+  url: 'https://commonwealthrobotics.com', // Your website URL
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
