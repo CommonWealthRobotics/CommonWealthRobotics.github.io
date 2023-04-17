ae6b10bb06754c8ff74558da0b0e914f373cb71c
Kevin Harrington <mad.hephaestus@gmail.com>
2023-04-17 12:42:21 -0400

    add ssl dir

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 sudo apt install rbenv
-rbenv install 2.7.6
+RUBY_CONFIGURE_OPTS="--with-openssl-dir=/usr/lib/ssl/" rbenv install 2.7.6
 bundle install;
 bundle exec nanoc view & bundle exec guard;
