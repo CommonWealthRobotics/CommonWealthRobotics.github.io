adc93d11d385a2969630c2feabc6d02047a637f2
Kevin Harrington <mad.hephaestus@gmail.com>
2023-04-17 12:49:07 -0400

    upadated ruby install

--- launch.sh
+++ launch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
-sudo apt install rbenv
-RUBY_CONFIGURE_OPTS="--with-openssl-dir=/usr/lib/ssl/" rbenv install 2.7.6
+sudo apt install gnupg2
+gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable
+rvm pkg install openssl
+rvm install 2.7.6 --with-openssl-dir=$HOME/.rvm/usr
 bundle install;
 bundle exec nanoc view & bundle exec guard;
