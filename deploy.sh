52b002aec0c0b22ad7ed1ce67f97b32ac1ff244b
Kevin Harrington <mad.hephaestus@gmail.com>
2025-04-13 15:55:59 -0400

    make sure the updated compile is added and pushed to the deploy branch

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+git log -1
+git config user.email "mad.hephaestus@gmail.com"
+git config user.name "Kevin Harrington"
+git config pull.rebase false
+git config core.mergeoptions --no-edit
 set -e 
 mv docs/maven/ ./maven/
 rm -rf docs
@@ -11,3 +15,9 @@ mv ./maven/ docs/
 set +e
 
 echo "commonwealthrobotics.com" >./docs/CNAME
+
+git add --all .
+
+git commit -a -m"Update for deploy"
+
+git push --force
